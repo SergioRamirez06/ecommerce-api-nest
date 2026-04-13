5389acafd9ba95bb97b548566697bf7945e316c5
sergioramirez06 <sergio200206mayo@gmail.com>
2026-04-13 17:52:00 -0500

    Configuracion de docker compose, conexion a la base de datos e integracion de ORM

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { join } from 'path';
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    
+    ServeStaticModule.forRoot({ 
+      rootPath: join(__dirname,'..','public'), 
+    }),
+    
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: +!process.env.PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    
+    ProductsModule,
+    
+    CommonModule 
+  ],
+})
+export class AppModule {}
---
+++ src/products/products.service.ts
@@ -0,0 +1,119 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+import { validate as isUuId } from 'uuid'
+
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { Product } from './entities/product.entity';
+
+
+@Injectable()
+export class ProductsService {
+
+  private readonly logger =  new Logger('ProductsService')
+
+
+  constructor(
+
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+
+    try {
+      const product = this.productRepository.create(createProductDto)
+      await this.productRepository.save( product )
+
+      return product;
+
+
+    } catch (error) {
+      this.handleDBErrors(error)
+    }
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+
+    const { limit = 10, offset = 0 } = paginationDto
+
+    const product = await this.productRepository.find({
+      take: limit,
+      skip: offset,
+
+    })
+    return product;
+  }
+
+  async findOne(term: string) {
+
+    let product: Product | null;
+
+    if( isUuId(term) ) {
+      product = await this.productRepository.findOneBy({ id: term })
+    }else {
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder.where('UPPER(title) =:title or slug =:slug', {
+        title: term.toUpperCase(),
+        slug: term.toLowerCase(),
+      }).getOne()
+    }
+
+    if( !product ) {
+      throw new NotFoundException(`Product with ${term} not found`)
+    } 
+
+    return product;
+  }
+
+   async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id: id,
+      ...updateProductDto
+    })
+
+    if( !product ) {
+        throw new NotFoundException(`Product with id:  ${id} not found.`)
+    }
+
+    try {
+      await this.productRepository.save( product );
+      return product;
+
+    } catch (error) {
+      this.handleDBErrors(error)
+    }
+  }
+
+  async remove(id: string) {
+    
+    const product = await this.findOne(id)
+    await this.productRepository.remove(product);
+
+    if( !product ) {
+      throw new NotFoundException(`Product with id: ${id} not found`)
+    }
+    return `Product with ${id} deleted`;
+  }
+
+  private handleDBErrors( error: any ) {
+    if( error.code === '23502' ) {
+      throw new BadRequestException(`Product with ${error.detail} already exists`)
+    }
+
+    if( error.code === '23505' ) { 
+      throw new BadRequestException(error.detail)
+    }
+
+    if( error.code === '22P02' ) {
+      throw new BadRequestException(`Product with id: ${error.value} not found`)
+    }
+
+    this.logger.error(error)
+    // console.log(error)
+    throw new InternalServerErrorException('Unexpected error, check server logs')
+  }
+}
